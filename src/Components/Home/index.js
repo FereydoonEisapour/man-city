c304feb24e7549a3d77e4437e1d06a017f4c99ec
Fereydoon     Eisapour <epfereydoon@gmail.com>
2021-03-07 20:14:47 +0330

    add  promotion

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,12 +1,14 @@
 import Featured from './Featured'
 import HomeMatches from './Matches'
 import MeetPlayers from './MeetPlayers'
+import Promotion from './Promotion'
 const Home = () => {
     return (
         <div className=" bck_blue">
             <Featured />
             <HomeMatches />
             <MeetPlayers />
+            <Promotion />
         </div>
 
     );
